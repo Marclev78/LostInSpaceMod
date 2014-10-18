a8e5ef5869f096f960373b7e0c89af9558630179
Marc Levin <marclev78@gmail.com>
2014-10-18 23:34:18 +0100

    Initial Commit

---
+++ LostInSpaceKspMod/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("LostInSpaceKspMod")]
+[assembly: AssemblyDescription("This mod enables grand tour and interstellar rescue contracts.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mod created by Marc Levin")]
+[assembly: AssemblyProduct("LostInSpaceKspMod")]
+[assembly: AssemblyCopyright("Public domain, do with this as you like.")]
+[assembly: AssemblyTrademark("Public domain, do with this as you like.")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
